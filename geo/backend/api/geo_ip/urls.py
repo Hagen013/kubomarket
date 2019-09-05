29f4b134cadb6da319c10e0d8ffdc71eb7a77e34
= <=>
2019-09-05 19:51:15 +0300

    345345345

--- geo/backend/api/geo_ip/urls.py
+++ geo/backend/api/geo_ip/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import url, include
 
-from .views import GeoIpAPIView
+from .views import GeoIpAPIView, GeoIpCoordinatesAPIView
 
 geo_ip_urls = (
     [
-        url(r'^$',
-            GeoIpAPIView.as_view()
-            ),
+        url(r'^$', GeoIpAPIView.as_view()),
+        url(r'^coordinates/$', GeoIpCoordinatesAPIView.as_view()),
     ],
     'geo_ip'
 )
--- geo/backend/api/geo_ip/views.py
+++ geo/backend/api/geo_ip/views.py
@@ -1,11 +1,13 @@
 import ipaddress
 from django.conf import settings
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404
 
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from .serializers import GeoIpSerializer
+from .serializers import GeoIpSerializer, GeoIpExtendedSerializer
 from geo_ip.models import GeoIp
 
 
@@ -29,3 +31,24 @@ class GeoIpAPIView(APIView):
         uwsgi серверу, тот в свою очередь передаёт её django
         """
         return request.META.get('REMOTE_ADDR')
+
+
+class GeoIpCoordinatesAPIView(APIView):
+
+    def get(self, request):
+        kladr_code = request.GET.get('kladr_code', '7700000000000')
+        if kladr_code == '7700000000000':
+            return Response({
+                'kladr_code': '7700000000000',
+                'kladr_name': 'Москва',
+                'latitude': 55.755787,
+                'longitude': 37.617634
+            })
+        else:
+            try:
+                
+                city = GeoIp.objects.filter(kladr_code=kladr_code)[0]
+                serializer = GeoIpExtendedSerializer(city)
+                retur Response(serializer.data)
+            except (IndexError, ObjectDoesNotExist):
+                raise Http404
